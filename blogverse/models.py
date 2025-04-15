ef3c9b81a071e93606b3a95580ca97716a389685
Taniya tandon <taniya3067.beai24@chitkara.edu.in>
2025-04-15 22:23:57 +0530

    Blog

--- blogverse/models.py
+++ blogverse/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     profession = models.CharField(max_length=100)
     gender = models.CharField(max_length=6)
